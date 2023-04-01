a12f05b2a660a4fea5a542e54679455e6e07ed41
scutFrankyWu <2378648054@qq.com>
2023-04-01 15:27:47 +0800

    my post and my reply

--- src/main/java/com/franky/community/control/AnalyseController.java
+++ src/main/java/com/franky/community/control/AnalyseController.java
@@ -1,14 +1,18 @@
 package com.franky.community.control;
 
 import com.franky.community.service.AnalyseService;
+import com.franky.community.tool.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class AnalyseController {
@@ -25,23 +29,27 @@ public class AnalyseController {
 
     // 统计网站UV
     @RequestMapping(path = "/data/uv", method = RequestMethod.POST)
+    @ResponseBody
     public String getUV(@DateTimeFormat(pattern = "yyyy-MM-dd") Date start,
                         @DateTimeFormat(pattern = "yyyy-MM-dd") Date end, Model model) {
         long uv = analyseService.calculateUV(start, end);
-        model.addAttribute("uvResult", uv);
+        Map<String,Object>map = new HashMap<>();
+        map.put("uvResult", uv);
         model.addAttribute("uvStartDate", start);
         model.addAttribute("uvEndDate", end);
-        return "forward:/data";
+        return CommunityUtil.getJSONString(0,null,map);
     }
 
     // 统计活跃用户
     @RequestMapping(path = "/data/dau", method = RequestMethod.POST)
+    @ResponseBody
     public String getDAU(@DateTimeFormat(pattern = "yyyy-MM-dd") Date start,
                          @DateTimeFormat(pattern = "yyyy-MM-dd") Date end, Model model) {
         long dau = analyseService.calculateDAU(start, end);
-        model.addAttribute("dauResult", dau);
+        Map<String,Object>map = new HashMap<>();
+        map.put("dauResult", dau);
         model.addAttribute("dauStartDate", start);
         model.addAttribute("dauEndDate", end);
-        return "forward:/data";
+        return CommunityUtil.getJSONString(0,null,map);
     }
 }
--- src/main/resources/static/js/data.js
+++ src/main/resources/static/js/data.js
@@ -1,3 +1,50 @@
-function calUV(start,end){
+function calUV(){
+    var start = document.getElementById("uvstart").value;
+    var end = document.getElementById("uvend").value;
+    if (end == "" || start == ""){
+        alert("日期未选中！");
+        return;
+    }
+    if (end < start) {
+        alert("结束日期不得早于开始日期");
+        return;
+    }
+    $.post(
+        CONTEXT_PATH + "/data/uv",
+        {"start": start, "end": end},
+        function (data) {
+            data = $.parseJSON(data);
+            if (data.code === 0) {
+                //window.location.reload(); // 刷新页面
+                $(uvres).text(data.uvResult);
+            } else {
+                alert(data.msg);
+            }
+        }
+        );
+}
 
+function calDAU(){
+    var start = document.getElementById("daustart").value;
+    var end = document.getElementById("dauend").value;
+    if (end == "" || start == ""){
+        alert("日期未选中！");
+        return;
+    }
+    if (end < start) {
+        alert("结束日期不得早于开始日期");
+        return;
+    }
+    $.post(
+        CONTEXT_PATH + "/data/dau",
+        {"start": start, "end": end},
+        function (data) {
+            data = $.parseJSON(data);
+            if (data.code === 0) {
+                //window.location.reload(); // 刷新页面
+                $(daures).text(data.dauResult);
+            } else {
+                alert(data.msg);
+            }
+        });
 }
